01ab952f2ceb35e64ff963646d87552167a08e80
Vince Zeng <vince.zeng0217@gmail.com>
2021-09-23 11:17:41 -0700

    Added codes

--- zeng_vincent_p1q1.py
+++ zeng_vincent_p1q1.py
@@ -1,3 +1,12 @@
 # Qiuz Creation Activity
 
 # Quiz has 5 questions the user will answer. It will keep track of score and give a final result.
+print("3" == "3")
+print(" 3" == "3")
+print(3 < 4)
+print(3 < 10)
+print("3" < "4")
+print("3" < "10")
+print( (2 == 2) == "True" )
+print( (2 == 2) == True )
+print(3 < int("3"))
