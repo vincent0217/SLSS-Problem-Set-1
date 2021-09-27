f7e8f3ec7d3f77ab45003c5bd0bc635b1cdd06ae
Vince Zeng <vince.zeng0217@gmail.com>
2021-09-27 12:46:32 -0700

    Inserted the first question.

--- zeng_vincent_p1q1.py
+++ zeng_vincent_p1q1.py
@@ -1,15 +1,19 @@
 # Quiz Creation Activity
 
+import time
+
 # Quiz has 5 questions the user will answer. It will keep track of score and give a final result.
-print("3" == "3")
-print(" 3" == "3")
-print(3 < 4)
-print(3 < 10)
-print("3" < "4")
-print("3" < "10")
-print( (2 == 2) == "True" )
-print( (2 == 2) == True )
-print(3 < int("3"))
 
 # Introduction
-print("Welcome to the Qwiztown!!!")
+print("Welcome to the Qwiztown!!! \nHere you will be taught the basics to survive in school.")
+time.sleep(1.5)
+print("Good luck.")
+time.sleep(2)
+
+# 1. a number as an answer (e.g., What is 1+1?)
+print("Your first task is to solve an elementary level math question.")
+answer = input(f"What is 1 + 1: ")
+if int(answer) == 2:
+    print("Super!")
+else:
+    print("Better luck next time!")
