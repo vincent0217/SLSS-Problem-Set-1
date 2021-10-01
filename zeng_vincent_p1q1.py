446137ce3a9a541f6cca010cf6a2161143eeefec
Vince Zeng <vince.zeng0217@gmail.com>
2021-10-01 12:16:56 -0700

    Added a second question.

--- zeng_vincent_p1q1.py
+++ zeng_vincent_p1q1.py
@@ -7,7 +7,9 @@ import time
 # Introduction
 print("Welcome to the Qwiztown!!! \nHere you will be taught the basics to survive in school.")
 time.sleep(1.5)
-print("Good luck.")
+user_name = input("Print your user name: ")
+
+print(f"Good luck {user_name}!\n")
 time.sleep(2)
 
 # 1. a number as an answer (e.g., What is 1+1?)
@@ -17,3 +19,19 @@ if int(answer) == 2:
     print("Super!")
 else:
     print("Better luck next time!")
+print("")
+time.sleep(1.5)
+
+# Transition
+print("Are you ready for the next question? HERE WE GO!")
+time.sleep(1.5)
+print("")
+
+# 2. text (e.g. What is Harry Potter's last name?)
+initials = input(f"What is Michael Jordan's initials: ").lower().strip(".,?!")
+
+if str(initials) == "mj":
+    print(f"Fantastic!")
+else:
+    print(f"C'mon now")
+print("")
