5ef0c564033a8a122ea56e62dfc79930bcc6d974
Vince Zeng <vince.zeng0217@gmail.com>
2021-10-04 12:17:35 -0700

    Added a third question.

--- zeng_vincent_p1q1.py
+++ zeng_vincent_p1q1.py
@@ -15,12 +15,12 @@ time.sleep(2)
 # 1. a number as an answer (e.g., What is 1+1?)
 print("Your first task is to solve an elementary level math question.")
 answer = input(f"What is 1 + 1: ")
-if int(answer) == 2:
+if str(answer) == "2":
     print("Super!")
 else:
     print("Better luck next time!")
 print("")
-time.sleep(1.5)
+time.sleep(1)
 
 # Transition
 print("Are you ready for the next question? HERE WE GO!")
@@ -28,10 +28,27 @@ time.sleep(1.5)
 print("")
 
 # 2. text (e.g. What is Harry Potter's last name?)
-initials = input(f"What is Michael Jordan's initials: ").lower().strip(".,?!")
+initials = input(f"What is [/bold red]Michael Jordan's initials: ").lower().strip(".,?!")
 
 if str(initials) == "mj":
     print(f"Fantastic!")
 else:
     print(f"C'mon now")
 print("")
+
+# Transition to question 3
+print(f"Here comes question 3 {user_name}!")
+
+# 3. a selection (Which of these choices are correct? A, B, or C?)
+answer = input(f"What is the colour of the sky? Select one of the options below.\n"
+               f"\nA. Blue\nB. Red\nC. purple\n").lower().strip("!?.,")
+a = "blue"
+b = "red"
+c = "purple"
+
+if answer == a:
+    print("Wow, you rock!")
+elif answer == b:
+    print("Dude, what kind of sky is red?")
+else:
+    print("say no more. We live in different universes.")
